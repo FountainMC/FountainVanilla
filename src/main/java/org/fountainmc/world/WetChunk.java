ec5e713070a1219a2aad0ed3e2c868a4fb1e7144
Jadon Fowler <jadonflower@gmail.com>
2016-06-11 03:20:55 -0700

    Call BlockBreakEvent and add WetWorld
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/main/java/org/fountainmc/world/WetChunk.java
+++ src/main/java/org/fountainmc/world/WetChunk.java
@@ -2,22 +2,21 @@ package org.fountainmc.world;
 
 import org.fountainmc.api.world.Chunk;
 import org.fountainmc.api.world.World;
-import org.fountainmc.api.world.Block;
+import org.fountainmc.api.world.BlockPosition;
 
 public class WetChunk implements Chunk {
-    
+
     private final net.minecraft.world.chunk.Chunk chunk;
     private final net.minecraft.world.World world;
     private final int x;
     private final int z;
-    
+
     public WetChunk(net.minecraft.world.chunk.Chunk chunk) {
         this.chunk = chunk;
         this.world = chunk.getWorld();
         this.x = chunk.xPosition;
         this.z = chunk.zPosition;
     }
-    
 
     @Override public int getX() {
         return x;
@@ -28,15 +27,14 @@ public class WetChunk implements Chunk {
     }
 
     @Override public World getWorld() {
-        return null;
+        return new WetWorld(world);
     }
 
-    @Override public Block getBlockAt(int x, int y, int z) {
-        return new Block(getWorld(), x, y, z);
+    @Override public BlockPosition getBlockAt(int x, int y, int z) {
+        return new BlockPosition(getWorld(), x, y, z);
     }
-    
+
     public net.minecraft.world.chunk.Chunk getHandle() {
         return chunk;
     }
-
 }
--- src/main/java/org/fountainmc/world/WetWorld.java
+++ src/main/java/org/fountainmc/world/WetWorld.java
@@ -1,12 +1,27 @@
 package org.fountainmc.world;
 
 import org.fountainmc.api.world.World;
-import org.fountainmc.api.world.Block;
+import org.fountainmc.api.world.BlockPosition;
+import org.fountainmc.api.world.Chunk;
 
 public class WetWorld implements World {
 
-    @Override public Block getBlock(int x, int y, int z) {
-        return null;
+    private net.minecraft.world.World world;
+    
+    public WetWorld(net.minecraft.world.World world) {
+        this.world = world;
+    }
+
+    @Override public String getName() {
+        return "world";
+    }
+
+    @Override public Chunk getChunk(int x, int y) {
+        return new WetChunk(world.getChunkFromChunkCoords(x, y));
+    }
+
+    @Override public BlockPosition getBlockAt(int x, int y, int z) {
+        return new BlockPosition(this, x, y, z);
     }
 
 }
